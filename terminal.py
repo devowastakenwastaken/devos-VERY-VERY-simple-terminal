5484d98b4e685450fdf6e6a72bec09ec2b02e174
devowastakenwastaken <84292597+devowastakenwastaken@users.noreply.github.com>
2024-11-12 13:19:46 +0000

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,6 +1,31 @@
 import sys, os, shutil, random, platform
 from random import randint
 
+def terminal():
+    while True:
+        terminalinput = input('\\terminal.py >>> $ ')
+        if terminalinput == '.echo':
+            echo()
+        if terminalinput == '.platform':
+            plat()
+        if terminalinput == '.disk':
+            diskspace()
+        if terminalinput == '.exit':
+            exit()
+
+startup = input('start terminal? [Y/N] >>> ')
+if startup == 'Y':
+    terminal()
+if startup == 'y':
+    terminal()
+if startup == 'yes':
+    terminal()
+if startup == 'Yes':
+    terminal()
+else:
+    print('ERROR: reply to input \'startup\' - unspecified response')
+    exit()
+
 def echo():
     echo = input('\\terminal.py - echo input >>> $ ')
     print(f'\\terminal.py - echo >>> $ {echo}')
@@ -14,12 +39,3 @@ def diskspace():
     print("Total: %d GiB" % (total // (2**30)))
     print("Used: %d GiB" % (used // (2**30)))
     print("Free: %d GiB" % (free // (2**30)))
-
-while True:
-    terminalinput = input('\\terminal.py >>> $ ')
-    if terminalinput == '.echo':
-        echo()
-    if terminalinput == '.platform':
-        plat()
-    if terminalinput == '.disk':
-        diskspace()
