83d7ac8b4a99874cc16454679e1d770f5bc91658
devowastakenwastaken <84292597+devowastakenwastaken@users.noreply.github.com>
2024-11-12 14:00:33 +0000

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,32 +1,7 @@
 import sys, os, shutil, random, platform
 from random import randint
-
-def terminal():
-    while True:
-        terminalinput = input('\\terminal.py >>> $ ')
-        if terminalinput == '.echo':
-            echo()
-        if terminalinput == '.platform':
-            plat()
-        if terminalinput == '.disk':
-            diskspace()
-        if terminalinput == '.exit':
-            exit()
-        if terminalinput == '.installgit':
-            pass
-
-startup = input('start terminal? [Y/N] >>> ')
-if startup == 'Y':
-    terminal()
-if startup == 'y':
-    terminal()
-if startup == 'yes':
-    terminal()
-if startup == 'Yes':
-    terminal()
-else:
-    print('ERROR: reply to input \'startup\' - unspecified response')
-    exit()
+import tkinter as tk
+from tkinter import *
 
 def echo():
     echo = input('\\terminal.py - echo input >>> $ ')
@@ -41,6 +16,46 @@ def diskspace():
     print("Total: %d GiB" % (total // (2**30)))
     print("Used: %d GiB" % (used // (2**30)))
     print("Free: %d GiB" % (free // (2**30)))
+def guiver():
+    root = tk.Tk()
+    root.geometry('1000x650')
+
+
+
+    root.mainloop()
+def terminal():
+    while True:
+        terminalinput = input('\\terminal.py >>> $ ')
+        if terminalinput == '.echo':
+            echo()
+        if terminalinput == '.platform':
+            plat()
+        if terminalinput == '.disk':
+            diskspace()
+        if terminalinput == '.quit':
+            exit()
+        if terminalinput == '.gui':
+            guiver()
+        if terminalinput == '.?':
+            pass
+
+startup = input('start terminal? [Y/N] >>> ')
+if startup == 'Y':
+    terminal()
+if startup == 'y':
+    terminal()
+if startup == 'yes':
+    terminal()
+if startup == 'Yes':
+    terminal()
+if startup == 'n':
+    exit()
+else:
+    print('ERROR: reply to input \'startup\' - unspecified response')
+    exit()
+
+
+
 
 # Objectives:
 # Add disk function ✔
