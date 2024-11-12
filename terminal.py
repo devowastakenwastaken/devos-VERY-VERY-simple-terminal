43bdd437bf0af6b8bc888eeb4771707f893ecde9
devowastakenwastaken <84292597+devowastakenwastaken@users.noreply.github.com>
2024-11-12 13:00:36 +0000

    added .echo, .plat and .disk

--- terminal.py
+++ terminal.py
@@ -1,1 +1,25 @@
+import sys, os, shutil, random, platform
+from random import randint
 
+def echo():
+    echo = input('\\terminal.py - echo input >>> $ ')
+    print(f'\\terminal.py - echo >>> $ {echo}')
+def plat():
+    print(f'\\terminal.py - platform.platform >>> $ {platform.platform()} \n'
+          f'\\terminal.py - platform.system >>> $ {platform.system()} \n'
+          f'\\terminal.py - platform.release >>> $ {platform.release()}')
+def diskspace():
+    total, used, free = shutil.disk_usage("/")
+
+    print("Total: %d GiB" % (total // (2**30)))
+    print("Used: %d GiB" % (used // (2**30)))
+    print("Free: %d GiB" % (free // (2**30)))
+
+while True:
+    terminalinput = input('\\terminal.py >>> $ ')
+    if terminalinput == '.echo':
+        echo()
+    if terminalinput == '.platform':
+        plat()
+    if terminalinput == '.disk':
+        diskspace()
